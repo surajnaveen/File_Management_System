2e9fb609c92afcc41a3f43946d23ccd8578c1ecc
surajnaveen <suraj0nb@gmail.com>
2024-08-24 11:27:25 +0530

    styling dashbord and connect firebase

---
+++ firebase/app.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRD6RCiqT46VF2HO0c7kvOuP1uLlCnZ6c",
+  authDomain: "unifile-83068.firebaseapp.com",
+  projectId: "unifile-83068",
+  storageBucket: "unifile-83068.appspot.com",
+  messagingSenderId: "598820332257",
+  appId: "1:598820332257:web:706fc4c75e0a7b9306cda0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const Firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export {auth,Firestore,storage};
